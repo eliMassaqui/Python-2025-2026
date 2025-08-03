5966a51bdb7a3b9833aaf2557ac1b790187e5cd8
MELLO <elimassaqui@gmail.com>
2025-08-03 19:36:56 -0300

    Lembrei

--- Origem/Hello.py
+++ Origem/Hello.py
@@ -1,1 +1,19 @@
-print("Hello Python")
+## Chamar uma função:
+
+def diz_ola(nome):
+    return f"Olá Mundo, meu nome é {nome}!"
+
+
+retotorno_1 = diz_ola("Adriano")
+retotorno_2 = diz_ola(nome="João")
+resultado = retotorno_1, retotorno_2
+
+global printando
+printando = "1234"
+
+for n in printando:
+    print(resultado)
+    print(retotorno_1)
+    print(retotorno_2)
+    print(printando)
+    print(n)
