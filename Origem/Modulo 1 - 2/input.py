edef464d4f9dc2bb287ac98ca9eeea2538b3d3f6
MELLO <elimassaqui@gmail.com>
2025-08-04 18:52:33 -0300

    Tudo é função kkk

--- Origem/Modulo 1 - 2/input.py
+++ Origem/Modulo 1 - 2/input.py
@@ -1,3 +1,11 @@
-nome = input("Digite o seu nome: ")
+# nome = input("Digite o seu nome: ")
+# print(nome)
 
-print(nome)
+#Chamando input com mensagens predefinidas pra usuário.
+numero = input("Digite o number: ")
+expoente = input("Digite o expoente: ")
+
+# Calculando, mas pra evitar erro dde que: não pode calcular sequencia de valores não-int
+# Que nesse caso chamamos dentro da função int que fez a transformção por baixo dos panos.
+resultadoExpoente = int(numero) ** int(expoente)
+print(resultadoExpoente)
