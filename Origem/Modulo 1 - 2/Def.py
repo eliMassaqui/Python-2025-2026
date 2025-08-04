d81a9afecff3d52dc09159883b3222bea25a0dc6
MELLO <elimassaqui@gmail.com>
2025-08-04 19:38:30 -0300

    Eestamos na def
    # Palavara reservada chamda def pra usar função!
    # Nome da função, não pode ser igual a uma palabra reservada!
    # A função pode ter parametros, como seus seus espaço primitivo que vária!

--- Origem/Modulo 1 - 2/Def.py
+++ Origem/Modulo 1 - 2/Def.py
@@ -1,3 +1,9 @@
-# Palavara reservada chamda def
-
+# Palavara reservada chamda def pra usar função!
+# Nome da função, não pode ser igual a uma palabra reservada!
+# A função pode ter parametros, como seus seus espaço primitivo que vária!
 print("Sobre funções!")
+
+def function():
+    return print('Eestamos na def')
+
+function()
