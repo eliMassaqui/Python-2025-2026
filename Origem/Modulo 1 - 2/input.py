d30ba36b65de935d36dca32926959af67484bba9
MELLO <elimassaqui@gmail.com>
2025-08-04 19:00:49 -0300

    Funcionamento do PC

--- Origem/Modulo 1 - 2/input.py
+++ Origem/Modulo 1 - 2/input.py
@@ -1,13 +1,21 @@
 # nome = input("Digite o seu nome: ")
 # print(nome)
 
+#Basicamente o funcionamento do computador como:
+
+# Entrada De Dados:
+
 #Chamando input com mensagens predefinidas pra usuário.
 numero = input("Digite o number: ")
 expoente = input("Digite o expoente: ")
 
+# Preocessamento De Dados:
+
 # Calculando, mas pra evitar erro dde que: não pode calcular sequencia de valores não-int
 # Que nesse caso chamamos dentro da função int que fez a transformção por baixo dos panos.
 resultadoExpoente = int(numero) * int(expoente)
+
+# Saída De Dados:
 
 # Agora fazemos o tratamento, agora na função de interpolação de strings
 # Não esquecer do f antes da string
