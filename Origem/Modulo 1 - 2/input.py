b542bfffd4e23de27b1e1ca475766d257bc6f7ee
MELLO <elimassaqui@gmail.com>
2025-08-04 18:57:37 -0300

    Para alguns tratamento especiais assim como a interpolação de strings f"antes da string"

--- Origem/Modulo 1 - 2/input.py
+++ Origem/Modulo 1 - 2/input.py
@@ -7,5 +7,8 @@ expoente = input("Digite o expoente: ")
 
 # Calculando, mas pra evitar erro dde que: não pode calcular sequencia de valores não-int
 # Que nesse caso chamamos dentro da função int que fez a transformção por baixo dos panos.
-resultadoExpoente = int(numero) ** int(expoente)
-print(resultadoExpoente)
+resultadoExpoente = int(numero) * int(expoente)
+
+# Agora fazemos o tratamento, agora na função de interpolação de strings
+# Não esquecer do f antes da string
+print(f"O resultado do {numero} elevado a {expoente} é igual: {resultadoExpoente}")
